0ce554f3961afbe163d0aaa68a295767ffea291b
Charlie Briggs <charlie.briggs@ft.com>
2019-06-05 15:24:49 +0100

    2 second work timeout
    This is extremely unlikely. Something is not working here

--- internal/handlers/collect.go
+++ internal/handlers/collect.go
@@ -58,7 +58,7 @@ func Collect(httpClient *http.Client) http.Handler {
 		hardTimeout := time.Duration(hardTimeoutSeconds) * time.Second
 
 		// Offset substracted from the work timeout to allow work to finish before promhttp returns a 500
-		workTimeout := time.Duration(hardTimeoutSeconds-1) * time.Second
+		workTimeout := time.Duration(hardTimeoutSeconds-2) * time.Second
 
 		// Add the timeout to this request.
 		ctx, cancel := context.WithTimeout(context.Background(), workTimeout)
